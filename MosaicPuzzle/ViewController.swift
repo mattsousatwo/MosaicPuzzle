6315cbaafe6cef5f8cf156a11a9261adc46df426
Matt Sousa <mattsousatwo@gmail.com>
2019-05-20 15:45:59 -0400

    Initial Commit
    Recreating Gridy Project

---
+++ MosaicPuzzle/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MosaicPuzzle
+//
+//  Created by Matthew Sousa on 2019-05-20.
+//  Copyright © 2019 mattsousa. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(ciColor: .blue)
+        print("loaded")
+    }
+
+
+
+}
